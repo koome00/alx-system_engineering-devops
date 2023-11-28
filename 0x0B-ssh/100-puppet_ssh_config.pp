e3ae5a22ccadcc01ab274ad1ef0cb8a039392cab
koome <koomemc@gmail.com>
2023-11-28 13:34:41 -0800

    remove keys

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,9 +0,0 @@
-# configure client to remove password authentification
-file_line { 'passw_no_authentication':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-}
-file_line { 'passw_direction':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentifyFile ~/.ssh/school',
-}
