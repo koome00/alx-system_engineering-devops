ca1a3d525a84c143c78288b89cad0f658c435f0c
koome00 <koomemc@gmail.com>
2024-01-17 11:33:31 -0800

    check if subreddit exists

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,15 +11,27 @@ def number_of_subscribers(subreddit):
     """
     returns number of subscribers of a channel
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+    # check if subreddits exist
+        try:
+            # check if valid json response
+            r = response.json()
+            subscribers = r.get('data', {}).get('subscribers')
+            if subscribers is not None:
+                return subscribers
+            else:
+                return 9
+        except requests.exceptions.JSONDecodeError as e:
+            return 3
 
-    header = requests.utils.default_headers()
-    header.update({'User-Agent': 'Reddit Api'})
-
-    response = requests.get(url, headers=header)
-    r = response.json()
-
-    subscribers = r.get('data', None).get('subscribers')
-    if not subscribers:
-        return 0
-    return subscribers
+    elif response.status_code == 404:
+    # if subreddit does not exist
+        return 4
+    else:
+        return response.status_code
