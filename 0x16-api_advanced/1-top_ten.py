ac31c494ded8fac75c7f0551aaee3f1d2f73aa4f
koome <koomemc@gmail.com>
2024-01-17 23:15:54 +0300

    posts.get instead of r.get

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,7 +15,6 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
-    
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -23,11 +22,11 @@ def top_ten(subreddit):
             r = response.json()
             top = r.get('data', {}).get('children')
             for posts in top:
-                print(r.get('data').get('title'))
+                print(posts.get('data').get('title'))
         except requests.exceptions.JSONDecodeError as e:
             return None
     elif response.status_code == 404:
-    # if subreddit does not exist
+        # if subreddit does not exist
         return None
     else:
         return None
